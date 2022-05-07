45d98c0d05d12153a9d366cfe58ca0cc62e3ad5b
Anders Gustav Kensby <Anders.Gustav.Kensby@dnb.no>
2022-05-07 16:22:22 +0200

    testing

--- request-headers.go
+++ request-headers.go
@@ -1,10 +1,10 @@
-package requestheaders
+package middlewares
 
 import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetTraceIdMiddleware() gin.HandlerFunc {
+func setTraceId() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Set("X-Trace-Id", c.Request.Header.Get("X-Trace-Id"))
 		c.Next()
